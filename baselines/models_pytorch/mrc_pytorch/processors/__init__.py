5301fcde3fe910958cf0c25c690f8c62039dfc90
HITalexwang <hitalexwang@gmail.com>
2021-03-18 20:04:29 -0700

    add label_lists

--- baselines/models_pytorch/mrc_pytorch/processors/__init__.py
+++ baselines/models_pytorch/mrc_pytorch/processors/__init__.py
@@ -1,6 +1,11 @@
 from .c3_processor import (c3Processor, c3_collate_fn, load_and_cache_c3_examples)
 from .chid_processor import (chid_collate_fn, load_and_cache_chid_examples)
 
+label_lists = {
+		'chid': ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+    'c3': ["0", "1", "2", "3"],
+}
+
 example_loaders = {
 		'chid': load_and_cache_chid_examples,
     'c3': load_and_cache_c3_examples,
